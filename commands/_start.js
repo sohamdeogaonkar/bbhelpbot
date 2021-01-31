9e9ed36abf8807fd57b75803277448aff47dc8e3
sohamdeogaonkar <63729589+sohamdeogaonkar@users.noreply.github.com>
2021-01-31 06:07:38 +0100

    Create _start.js

---
+++ commands/_start.js
@@ -0,0 +1,12 @@
+/*CMD
+  command: /start
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  <<ANSWER
+This bot say hello to new users in chat.
+Add it to any group chat.
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
